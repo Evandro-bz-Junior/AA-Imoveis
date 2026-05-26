a27ef17ebae8bc4aa5f9f4682dc7da6d9612fa82
Evandro-bz-junior <evandrobzjunior@gmail.com>
2026-05-26 00:26:33 -0300

    fix upload image types

--- src/app/admin/edit-property/[id]/page.tsx
+++ src/app/admin/edit-property/[id]/page.tsx
@@ -15,7 +15,7 @@ export default function EditPropertyPage() {
 
     const [loading, setLoading] = useState(true)
 
-    const [files, setFiles] = useState<(File | string)[]>([])
+    const [files, setFiles] = useState<File[]>([])
 
     const [form, setForm] = useState({
         title: "",
